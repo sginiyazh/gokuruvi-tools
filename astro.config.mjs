7c4d2dfbcf243aab08d25f5724aa7d173c99fc3b
Sathish Ramalingam <rajamannargudisathish@gmail.com>
2026-07-29 11:15:39 +0400

    Add essential website pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://www.gokuruvi.com",
+
+  output: "server",
+
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
